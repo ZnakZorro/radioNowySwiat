7aa1786a3a113f699856bd8802646935177f7c30
Znak Zorro <ZnakZorro@users.noreply.github.com>
2021-09-18 08:54:15 +0200

    Update index.h

--- A1S/ESP32_ES8388_v1/index.h
+++ A1S/ESP32_ES8388_v1/index.h
@@ -1,59 +1,57 @@
 const char PAGE_HTML[] PROGMEM = R"=====(
-
 <!DOCTYPE html>
 <html lang="pl">
 <head>
 <meta charset="utf-8">
-<title>@radio I2S</title>
+<title>@radio RNS</title>
 <meta name="viewport" content="width=device-width,initial-scale=1">
 <meta name="mobile-web-app-capable" content="yes">
-<meta name="theme-color" content="#db5945">
+<meta name="theme-color" content="#0F2C4B">
 <style>
-html {font:normal 1.05em verdana}
-body {background-color: #152028; padding: 0.25em; color:white;}
+  /*#4B110F#8D9176#0F2C4B#E7E3C1#D67E2B*/
+html {font:normal 14pt verdana}
+body {background-color: #0F2C4B; padding: 0.25em; color:white;}
 .col {max-width: 600px;margin: auto; text-align:center;}
-h3 {margin-top:0;}
-#info,#czas {min-height: 1.5em;font-size:0.8em;}
+h3 {margin:0.3em 0;}
+#info,#czas {font-size:0.85em;font-family: monospace;}
+#info {min-height: 4.5em;}
 .btn {
-    background-color: #202945;
-    border-color: #000;
-    color: white;
-    padding: 1em 0.25em;
-    box-shadow: inset 0px 0px 3px 3px #16162f;
+  background-color: #28396f;
+  border-color: #000;
+  color: white;
+  padding: 0.5em 0.25em;
+  box-shadow: inset 0px 0px 1px 1px #16162f;
   border-radius: 0.75em;
   text-align: center;
 }
 .btn:active, .btn:visited, .active {
-    background-color: #2c395f;
-    border-color: #6079c7;
-    box-shadow: inset 0px 0px 3px 3px #4b4ba7;
+  background-color: #2c395f!important;
+  border-color: #6079c7;
+  box-shadow: inset 0px 0px 2px 2px #4b4ba7;
 }
 .btns {
   max-width: 1024px;
-  margin: 0.5em auto;
+  margin: 0.5em auto ;
   display: grid;
   grid-gap: 0.5em;
   grid-template-columns: repeat(auto-fit, minmax(120px, 1fr));
 }
-.btns-sm {
-  grid-template-columns: repeat(auto-fit, minmax(60px, 1fr));
-}
-.btns-bg {
-  grid-template-columns: repeat(auto-fit, minmax(80px, 1fr));
-}
-.btns-sm .btn {font-size:0.95em; background-color:#182345;}
-.btns.red button{background-color:#400;}
-.btns.blue button{background-color:#1b3070;}
+.btns-sm,.btn-sm {grid-template-columns: repeat(auto-fit, minmax(60px, 1fr));}
+.btns-bg {grid-template-columns: repeat(auto-fit, minmax(80px, 1fr));margin-top:1em;}
+
+.btns.red button{background-color:#4B110F;}
+
 div#app span {
-    width: 3em;
-    height: 1.3em;
-    background: white;
-    color: navy;
-    text-align: center;
-    padding: 0.3em;
-    margin: auto;
-    border-radius: 0.5em;
+  width: 3em;
+  height: 1.3em;
+  background: #E7E3C1;
+  color: navy;
+  text-align: center;
+  padding: 0.3em;
+  margin: auto;
+  border-radius: 0.5em;
 }
+span.sp5 {padding:1em; display: grid;grid-template-columns: repeat(auto-fit, minmax(80px, 1fr));border: 1px solid black; box-shadow: inset 0px 0px 2px 2px black;}
 </style>
 <script>
 const _$=e=>document.querySelector(e);
@@ -78,6 +76,7 @@ fetch(url+p)
 
 const btnActive=(lSTA)=>{
   _$$("div.st button").forEach((b)=>{if (b) b.classList.remove("active")});
+  console.log(_$("#st"+lSTA));
   if(lSTA>=0 && _$("#st"+lSTA)) _$("#st"+lSTA).classList.add("active");
 }
 const opisz=(o)=>{
@@ -98,29 +97,49 @@ const opisz=(o)=>{
 <body>
 
 <div class="col">
-  <h3>@radio I2S NŚ</h3>
+  <h3 title="ESP32 A1S I2S V2.2">@radio RNŚ</h3>
   <div id="info"></div>
   <div id="app">
-  <div class="btns btns-sm blue">
-    <button class="btn" onClick='sn("radio?s=m")'>Sta-</button>
-    <span id="sta"></span>
-    <button class="btn" onClick='sn("radio?s=p")'>Sta+</button>
+    <div class="btns btn-sm blue">
+      <button class="btn" onClick='sn("radio?v=m")'>Vol-</button>
+      <span id="vol"></span>
+      <button class="btn" onClick='sn("radio?v=p")'>Vol+</button>
+    </div>
+    <div class="btns btn-sm red">
+      <button class="btn" onClick='sn("radio?s=m")'>Sta-</button>
+      <span id="sta"></span>
+      <button class="btn" onClick='sn("radio?s=p")'>Sta+</button>
+    </div>   
   </div>
-  <div class="btns btns-sm red">
-    <button class="btn" onClick='sn("radio?v=m")'>Vol-</button>
-    <span id="vol"></span>
-    <button class="btn" onClick='sn("radio?v=p")'>Vol+</button>
-  </div>
-  </div>
-  <div class="btns btns-bg blue">
+  <div class="btns btns-bg btns-sm blue">
     <button onClick='sn("radio?n=0")' class="btn">Info</button>
     <button onClick='sn("radio?start=0")' class="btn">Mute</button>
     <button onClick='sn("radio?z=0")' class="btn">Reset</button>
   </div>
+  <div class="btns btns-bg btns-sm red st">
+    <button onClick='sn("radio?t=0")' class="btn" id="st0">TOK-FM</button>
+    <button onClick='sn("radio?t=1")' class="btn" id="st1">RNŚ</button>
+    <button onClick='sn("radio?t=2")' class="btn" id="st2">357</button>
+    <button onClick='sn("radio?t=3")' class="btn" id="st3">S-Jazz</button>
+    <button onClick='sn("radio?t=4")' class="btn" id="st4">S-Class</button>
+    <button onClick='sn("radio?t=5")' class="btn" id="st5">RMF-Cl</button>
+
+  </div>
 
 </div>
+  
+<br /><br /><br /> 
+<!--#4B110F#8D9176#0F2C4B#E7E3C1#D67E2B-->
+  <div class="col btns">
+    <span class="sp5" style="background:#4B110F">#4B110F</span>
+    <span class="sp5" style="background:#8D9176">#8D9176</span>
+    <span class="sp5" style="background:#0F2C4B">#0F2C4B</span>
+    <span class="sp5" style="background:#E7E3C1">#E7E3C1</span>
+    
+  </div>
 </body>
 </html>
+
 
 
 )=====";
